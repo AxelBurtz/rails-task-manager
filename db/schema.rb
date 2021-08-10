02835542e5b353321410c84c4da23d1fe780bb1c
axel burtz <axelburtz@gmail.com>
2021-08-10 14:52:41 +0200

    Exercice done

--- db/schema.rb
+++ db/schema.rb
@@ -15,10 +15,9 @@ ActiveRecord::Schema.define(version: 2021_08_10_095224) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
+    t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "completed"
-    t.boolean "#<ActiveRecord::ConnectionAdapters::SQLite3::TableDefinition:0x000055603a899c58>"
   end
 
 end
